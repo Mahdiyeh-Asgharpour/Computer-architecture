6d0ca5aa678fb5840368285a6e9d3a952e804af9
mahdiyeh-asgharpour <www.mahdiyeh.mazani@gmail.com>
2023-04-20 22:11:38 +0330

    Add files via upload

--- counter.v
+++ counter.v
@@ -1,22 +1,25 @@
-module counter(input clk,input x,output  a,output  b,output  c);
+module counter(input clk,input rst, input x,output  a,output  b,output  c);
   and(y1,!a,!b,!c);
   and(y2,a,b,c);
   or(ta,y1,y2);
-  tff1 tffa(clk,ta,a);
+  tff1 tffa(clk,ta,rst,a);
   and(y3,!c,!x);
   and(y4,x,c);
   or(tb,y3,y4);
-  tff1 tffb(clk,tb,b);
+  tff1 tffb(clk,tb,rst,b);
   assign tc=1;
-  tff1 tffc(clk,tc,c);
+  tff1 tffc(clk,tc,rst,c);
   
   endmodule
- module tff1 ( input clk, input t, output reg q);  
-  always @ (posedge clk) begin  
-    if (t==1)  
-		q<=!q;  
-    else  
-        q<=q;
+ module tff1 ( input clk, input t,input rst, output reg q);  
+  always @ (posedge clk) begin
+	if(rst)
+		q=0;
+	else
+		if (t==1)  
+			q<=!q;  
+		else  
+			q<=q;
   end  
  endmodule
  
